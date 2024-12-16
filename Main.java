209f06255dec73f79e834f2695bb8928c103df75
Mahfuzul Haque <fajilhaque125@gmail.com>
2024-12-17 01:16:15 +0600

    added main

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner nigga = new Scanner(System.in);
+        String str = nigga.nextLine();
+        
+        /*
+        1. A
+        2. p
+        3. p
+        4. L
+        .
+        .
+        */
+
+        for(int i = 1; i <= str.length(); i++) {
+            
+        }
+        System.out.print("1" + "1");    
+    }
+}
+
